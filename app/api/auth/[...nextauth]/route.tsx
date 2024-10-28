e88574d01011a7893d08d7e28f95be0dfac8dcd1
Jittu Varghese <oommenjittu@gmail.com>
2024-10-28 22:19:16 +0530

    Added Prelim Signin

---
+++ app/api/auth/[...nextauth]/route.tsx
@@ -0,0 +1,16 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export const GET = NextAuth(authOptions);
+export const POST = NextAuth(authOptions);
