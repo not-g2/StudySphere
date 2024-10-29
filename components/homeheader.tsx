6b2d447ceea853fb082a3245028d716677c3849e
Jittu Varghese <oommenjittu@gmail.com>
2024-10-29 11:22:41 +0530

    changes

--- components/homeheader.tsx
+++ components/homeheader.tsx
@@ -1,23 +1,39 @@
 "use client";
 
 import React from 'react';
-import { signIn } from 'next-auth/react';
+import { useSession, signIn, signOut } from 'next-auth/react';
 import '../app/output.css';
 
 function Header() {
+  const { data: session } = useSession();
+
   return (
     <header className="flex items-center justify-between p-4 bg-c1 text-c1">
       <div className="flex-grow"></div>
       <div className="flex space-x-4">
-        <button 
-          className="px-4 py-2 text-c5 bg-c4 rounded hover:bg-c3"
-          onClick={() => signIn()} // Add signIn functionality here
-        >
-          Sign In
-        </button>
-        <button className="px-4 py-2 text-c5 bg-c4 rounded hover:bg-c3">
-          Sign Up
-        </button>
+        {!session ? (
+          <div>
+            <button 
+              className="px-4 py-2 bg-c4 rounded hover:bg-c3"
+              onClick={() => signIn()} // Sign In functionality
+            >
+              Sign In
+            </button>
+            <button className="px-4 py-2 bg-c4 rounded hover:bg-c3">
+              Sign Up
+            </button>
+          </div>
+        ) : (
+          <div>
+            <img src={session.user?.image}></img>
+            <button 
+              className="px-4 py-2 bg-c4 rounded hover:bg-c3"
+              onClick={() => signOut()} // Sign Out functionality
+            >
+              Sign Out
+            </button>
+          </div>
+        )}
       </div>
     </header>
   );
