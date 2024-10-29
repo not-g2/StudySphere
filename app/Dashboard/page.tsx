c3298243e3d3bb2d5161601db2a2b31b71f931d1
AlbinSiby1 <albinsiby234@gmail.com>
2024-10-29 22:28:14 +0530

    Dashboard additions

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import DeadlinesList from '@/components/deadlines';
+import Dashheader from '@/components/dashheader';
+import Leaderboard from '@/components/leaderboard'; // Import the Leaderboard component
+import Calendar from '../../components/calender';
 import '../output.css';
 
-function page() {
+function Page() {
   return (
-    <div>
-        <DeadlinesList/>
+    <div >
+      <Dashheader />
+      <div className="p-4 flex">
+        {/* DeadlinesList with 28% width */}
+        <div className="w-28/100 p-4">
+          <DeadlinesList />
+        </div>
+        
+        {/* Leaderboard with 28% width */}
+        <div className="w-28/100 p-4">
+          <Leaderboard />
+        </div>
+        
+        {/* Calendar takes remaining width */}
+        <div className="flex-grow p-4">
+          <Calendar />
+        </div>
+      </div>
     </div>
   );
 }
 
-export default page;
+export default Page;
