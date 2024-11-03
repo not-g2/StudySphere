9bbbf212aadc6bfdbee704f066b828a9ec58407b
AlbinSiby1 <albinsiby234@gmail.com>
2024-11-03 18:32:59 +0530

    Bug Fixes

--- components/levelandexp.tsx
+++ components/levelandexp.tsx
@@ -7,9 +7,9 @@ interface LevelXPDisplayProps {
 
 const LevelXPDisplay: React.FC<LevelXPDisplayProps> = ({ level, xp }) => {
   return (
-    <div className="max-w-sm mx-auto text-center p-6 bg-black text-white border border-gray-700 rounded-lg shadow-lg">
+    <div className="max-w-sm mx-auto text-center p-6 bg-c3 text-white border border-gray-700 rounded-lg shadow-lg">
       <h2 className="text-2xl font-bold">Level</h2>
-      <p className="text-6xl font-extrabold text-blue-500">{level}</p>
+      <p className="text-6xl font-extrabold text-white">{level}</p>
       
       <div className="mt-6">
         <h3 className="text-xl font-bold">XP</h3>
---
+++ components/studentassignment.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+interface StudentAssignmentProps {
+  studentId: number;
+  studentName: string;
+  assignmentId: string;
+  approved: boolean; // Add approved prop
+  toggleApproved: () => void; // Add toggleApproved prop
+}
+
+const StudentAssignment: React.FC<StudentAssignmentProps> = ({
+  studentId,
+  studentName,
+  assignmentId,
+  approved,
+  toggleApproved,
+}) => {
+  return (
+    <li className="bg-c3 shadow-md rounded-lg p-4">
+      <h3 className="text-c5 font-semibold mb-2">{studentName}</h3>
+      <p className="text-c5 mb-2">
+        <strong>Assignment ID:</strong> {assignmentId}
+      </p>
+      <p className="text-c5 mb-2">
+        <strong>Student ID:</strong> {studentId}
+      </p>
+      <a
+        href="https://example.com/assignment-link"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-c1 hover:underline mb-4 inline-block"
+      >
+        View Assignment
+      </a>
+      <div className="flex items-center mt-4">
+        <input
+          type="checkbox"
+          checked={approved}
+          onChange={toggleApproved}
+          className="mr-2 h-5 w-5 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
+        />
+        <label className="text-c5">Mark as Corrected</label>
+      </div>
+    </li>
+  );
+};
+
+export default StudentAssignment;
