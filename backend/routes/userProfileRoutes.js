3884e8c666557abc868662e9f9a458cca486a0ae
Harsh Srivastava <harshsri1602@gmail.com>
2024-11-05 09:11:44 +0530

    slight bug fix

--- backend/routes/userProfileRoutes.js
+++ backend/routes/userProfileRoutes.js
@@ -7,7 +7,7 @@ const authMiddleware = require('../middleware/auth');  // Middleware to authenti
 // Get user profile by user ID
 router.get('/profile', authMiddleware, async (req, res) => {
     try {
-        const user = await User.findById(req.user.id).select('name email phone');
+        const user = await User.findById(req.user.userID).select('name email phoneNumber');
 
         if (!user) {
             return res.status(404).json({ msg: 'User not found' });
