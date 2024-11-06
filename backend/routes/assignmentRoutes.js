43b8f8ce3229811971b56fa6300579b57bf0ad3f
Jittu Varghese <oommenjittu@gmail.com>
2024-11-06 19:43:25 +0530

    FixedBug

--- backend/routes/assignmentRoutes.js
+++ backend/routes/assignmentRoutes.js
@@ -40,22 +40,24 @@ router.get("/func", authMiddleware, async (req, res) => {
         });
 
         // Return the sorted array of due dates
-        res.json({ dueDates });
-      } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch due dates' });
-      }
-})
+        res.json({ assignments });
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch due dates" });
+    }
+});
 
-// // Route 2: Get All Assignments for a Course
-// router.get('/course/:courseId', async (req, res) => {
-//     try {
-//         const assignments = await Assignment.find({ course: req.params.courseId });
-//         res.json(assignments);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'Server Error' });
-//     }
-// });
+// Route 2: Get All Assignments for a Course(Works)
+router.get("/course/:courseId", async (req, res) => {
+    try {
+        const assignments = await Assignment.find({
+            course: req.params.courseId,
+        });
+        res.json(assignments);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
+});
 
 // Route 3: Update Assignment Status(Works)
 router.put("/:id/status", async (req, res) => {
