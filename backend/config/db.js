f2737e08f3f4a4437f35b0a7e0dd08b5bafc7e68
Harsh Srivastava <harshsri1602@gmail.com>
2025-03-15 00:57:38 +0530

    Making the code more readable

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,36 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const connectDB = async() => {
-    try{
+// we can make this connectDB an IIFE (for now , we are importing this in the server.js file , and then running it there. This approach is better)
+
+// whenever u want to make an IIFE , make sure to start it with ";". This is because we might have forgotten to put ";" b4 hand , and that could create problem for our IIFE
+
+// this code could be written as in IIFE as follows :-
+/* ;(async () => {
+        try{
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            // this isnt needed anymore
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true
         })
 
         console.log(`MongoDB connected: ${conn.connection.host}`)
     } catch (error) {
         console.error(`Error: ${error.message}`)
         process.exit(1)
+    }
+    })()
+
+*/
+const connectDB = async() => {
+    // whenever u try to communicate with the backend , use try catch
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            // this isnt needed anymore
+            // useNewUrlParser: true, 
+            // useUnifiedTopology: true 
+        })
+
+        console.log(`MongoDB connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
     }
 }
 
