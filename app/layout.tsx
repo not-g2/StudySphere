b2e9214e27a43e5647900fb24df786aad4f4729a
Jittu Varghese <oommenjittu@gmail.com>
2025-03-15 12:22:04 +0400

    Fixed navbar showing during Provider Signin

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,23 +8,27 @@ import "./output.css";
 import { usePathname } from "next/navigation";
 
 export default function RootLayout({
-  children,
-  session,
+    children,
+    session,
 }: {
-  children: ReactNode;
-  session: any;
+    children: ReactNode;
+    session: any;
 }) {
-  const pathname = usePathname();
-  const isAdminRoute = pathname.startsWith("/admin");
+    const pathname = usePathname();
+    const isAdminRoute = pathname.startsWith("/admin");
+    const isAuthRedirect =
+        pathname.startsWith("/auth/google") ||
+        pathname.startsWith("/auth/github");
 
-  return (
-    <html lang="en">
-      <body>
-        <SessionProvider session={session}>
-          {isAdminRoute ? <AdminHeader /> : <Header />}
-          {children}
-        </SessionProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <SessionProvider session={session}>
+                    {!isAuthRedirect &&
+                        (isAdminRoute ? <AdminHeader /> : <Header />)}
+                    {children}
+                </SessionProvider>
+            </body>
+        </html>
+    );
 }
