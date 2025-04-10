21e5ef6a8529647ba9bb0cb80e5989e8d888938a
Harsh Srivastava <harshsri1602@gmail.com>
2025-04-10 21:18:10 +0530

    updated the logic for getting badge through pomodoro time

--- backend/helperfunction/badgeFromPomodoroTime.js
+++ backend/helperfunction/badgeFromPomodoroTime.js
@@ -6,7 +6,21 @@ function checkBadgeFromPomodoroTime(totalTimeSpent){
         604800 : '67e4099302cd398c11be688b',// 7 days in seconds
     }
 
-    return badgeId[totalTimeSpent] || null;
+    // apply binary search on the time spent to find the correct time (O(logN))
+    let arr = [1500,10800,86400,604800];
+    let left = 0 , right = arr.length;
+    while(left<right){
+        let mid = Math.floor((left+right)/2);
+        if(arr[mid]<totalTimeSpent){
+            left = mid+1;
+        }
+        else{
+            right = mid;
+        }
+    }
+
+    if (left === arr.length) left = arr.length - 1;
+    return badgeId[arr[left]];
 }
 
 module.exports = checkBadgeFromPomodoroTime;
