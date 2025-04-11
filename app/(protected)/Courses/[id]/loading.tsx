af4f81b0d5fc24fb92181b72164265e7a0f02090
Jittu Varghese <oommenjittu@gmail.com>
2025-04-11 10:47:48 +0530

    Made Loading.tsx client to help resolve bugs

--- app/(protected)/Courses/[id]/loading.tsx
+++ app/(protected)/Courses/[id]/loading.tsx
@@ -1,21 +1,24 @@
-import { CircularProgress, Box, Typography } from '@mui/material';
+"use client";
+import { CircularProgress, Box, Typography } from "@mui/material";
 
 const Loading: React.FC = () => {
-  return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      className="bg-c1" // Apply custom background color c1
-      sx={{ height: '100vh' }}
-    >
-      <CircularProgress />
-      <Typography variant="h6" sx={{ mt: 2, color: 'white' }}> {/* Set text color to white */}
-        Loading...
-      </Typography>
-    </Box>
-  );
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            className="bg-c1" // Apply custom background color c1
+            sx={{ height: "100vh" }}
+        >
+            <CircularProgress />
+            <Typography variant="h6" sx={{ mt: 2, color: "white" }}>
+                {" "}
+                {/* Set text color to white */}
+                Loading...
+            </Typography>
+        </Box>
+    );
 };
 
 export default Loading;
