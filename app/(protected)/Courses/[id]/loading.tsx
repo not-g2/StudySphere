d46940bd4e69e0ca47f1327b16e179f00554de35
Jittu Varghese <oommenjittu@gmail.com>
2025-04-11 15:47:46 +0530

    Fixes V7

--- app/(protected)/Courses/[id]/loading.tsx
+++ app/(protected)/Courses/[id]/loading.tsx
@@ -1,33 +1,25 @@
-// "use client";
-// import { CircularProgress, Box, Typography } from "@mui/material";
-// import dynamic from "next/dynamic";
+"use client";
+import { CircularProgress, Box, Typography } from "@mui/material";
+import dynamic from "next/dynamic";
 
-// const Loading: React.FC = () => {
-//     return (
-//         <Box
-//             display="flex"
-//             flexDirection="column"
-//             alignItems="center"
-//             justifyContent="center"
-//             className="bg-c1" // Apply custom background color c1
-//             sx={{ height: "100vh" }}
-//         >
-//             <CircularProgress />
-//             <Typography variant="h6" sx={{ mt: 2, color: "white" }}>
-//                 {" "}
-//                 {/* Set text color to white */}
-//                 Loading...
-//             </Typography>
-//         </Box>
-//     );
-// };
-
-// export default dynamic(() => Promise.resolve(Loading), { ssr: false });
-
-import React from "react";
-
-const page = () => {
-    return <div>page</div>;
+const Loading: React.FC = () => {
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            className="bg-c1" // Apply custom background color c1
+            sx={{ height: "100vh" }}
+        >
+            <CircularProgress />
+            <Typography variant="h6" sx={{ mt: 2, color: "white" }}>
+                {" "}
+                {/* Set text color to white */}
+                Loading...
+            </Typography>
+        </Box>
+    );
 };
 
-export default page;
+export default dynamic(() => Promise.resolve(Loading), { ssr: false });
