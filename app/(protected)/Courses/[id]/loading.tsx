928af90465ce8965e8b67d955cb024427b3d0bc6
Jittu Varghese <oommenjittu@gmail.com>
2025-04-11 17:16:03 +0530

    V17

--- app/(protected)/Courses/[id]/loading.tsx
+++ app/(protected)/Courses/[id]/loading.tsx
@@ -1,21 +1,14 @@
 "use client";
-// import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 
-// const Loading: React.FC = () => {
-//     return (
-//         <div className="flex flex-col items-center justify-center h-screen bg-c1">
-//             <div className="animate-spin rounded-full h-10 w-10 border-4 border-white border-t-transparent"></div>
-//             <p className="mt-4 text-white text-lg font-medium">Loading...</p>
-//         </div>
-//     );
-// };
-
-// export default dynamic(() => Promise.resolve(Loading), { ssr: false });
-
-import React from "react";
-
-const page = () => {
-    return <div>page</div>;
+const Loading: React.FC = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-c1">
+            <div className="animate-spin rounded-full h-10 w-10 border-4 border-white border-t-transparent"></div>
+            <p className="mt-4 text-white text-lg font-medium">Loading...</p>
+        </div>
+    );
 };
 
-export default page;
+export default dynamic(() => Promise.resolve(Loading), { ssr: false });
+
