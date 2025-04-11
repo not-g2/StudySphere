22b2f939a8357a70f2c4de3de14bf947adc40ffe
Jittu Varghese <oommenjittu@gmail.com>
2025-04-11 23:58:56 +0530

    final fix

--- app/(protected)/Goals/page.tsx
+++ app/(protected)/Goals/page.tsx
@@ -1,15 +1,7 @@
-// "use client";
-
-// import dynamic from "next/dynamic";
-
-// const clientPage = dynamic(() => import("./plzzzwork"), { ssr: false });
-
-// export default clientPage;
 "use client";
-import React from "react";
 
-const page = () => {
-    return <div>page</div>;
-};
+import dynamic from "next/dynamic";
 
-export default page;
+const clientPage = dynamic(() => import("./plzzzwork"), { ssr: false });
+
+export default clientPage;
