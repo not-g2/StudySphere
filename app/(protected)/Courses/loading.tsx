1031424406e272cb056a0977a08f481147031526
Jittu Varghese <oommenjittu@gmail.com>
2025-04-11 15:45:24 +0530

    Fixes V6

--- app/(protected)/Courses/loading.tsx
+++ app/(protected)/Courses/loading.tsx
@@ -23,3 +23,11 @@
 // };
 
 // export default dynamic(() => Promise.resolve(Loading), { ssr: false });
+
+import React from "react";
+
+const page = () => {
+    return <div>page</div>;
+};
+
+export default page;
